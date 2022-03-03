49344a42f238200867fb211646db1e9f1a1f631b
Ryan Policheri <Ryan-Poli90@outlook.com>
2022-03-02 18:21:22 -0600

    small progress

--- UnifiedDataExplorer/Services/Reporting/ReportProcessor.cs
+++ UnifiedDataExplorer/Services/Reporting/ReportProcessor.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
 using System.Threading.Tasks;
 using DotNetCommon.EventAggregation;
 using EIA.Domain.Model;
@@ -52,22 +54,70 @@ namespace UnifiedDataExplorer.Services.Reporting
             Asset asset = await _piClient.GetBySelfLink<Asset>(macleanLink);
             await _piClient.LoadAssetValues(asset);
 
-            string misoWindId = "EBA.MISO-ALL.NG.WND.HL";
-            Series series = await _eiaClient.GetSeriesByIdAsync(misoWindId);
+            IEnumerable<HourSummary> summaries = await BuildHourlySources();
+        }
+
+        private async Task<IEnumerable<HourSummary>> BuildHourlySources()
+        {
+            int numberOfHours = 24;
+            IEnumerable<HourlySource> sources = BuildSources();
+
+            ICollection<Series> seriesBySource = new List<Series>();
+            foreach (HourlySource source in sources)
+            {
+                Series sourceSeries = await _eiaClient.GetSeriesByIdAsync(source.SourceId);
+                seriesBySource.Add(sourceSeries);
+            }
+
+            ICollection<HourSummary> summaries = new List<HourSummary>();
+            DateTime startingHour = ParseHour(seriesBySource.First().Data.OrderByDescending(x => ParseHour(x.ColumnHeader)).First().ColumnHeader);
+
+            for (int i = 0; i < numberOfHours; i++)
+            {
+                HourSummary summary = new HourSummary();
+                summary.Hour = startingHour;
+
+                for(int j = 0; j < sources.Count(); j++)
+                {
+                    HourlySource source = sources.ElementAt(j);
+                    Series series = seriesBySource.ElementAt(j);
+                    SeriesData seriesData = series.Data.Where(x => ParseHour(x.ColumnHeader).Hour == startingHour.Hour).First();
+                    HourlySource hourlySource = source.ProduceHour(ParseHour(seriesData.ColumnHeader), seriesData.ColumnValue.Value);
+                    summary.Sources.Add(hourlySource);
+                }
+
+                summaries.Add(summary);
+                startingHour = startingHour.AddHours(-1);
+            }
+
+            return summaries;
+        }
+
+        private DateTime ParseHour(string hourAsString)
+        {
+            CultureInfo us = new CultureInfo("en-US");
+            return DateTime.ParseExact(hourAsString, "yyyyMMddTHH-mm", us);
         }
 
         private IEnumerable<HourlySource> BuildSources()
-        {
+        {//https://www.eia.gov/tools/faqs/faq.php?id=74&t=11
             ICollection<HourlySource> sources = new List<HourlySource>();
             sources.Add(new HourlySource { Source = "Wind", SourceId = "EBA.MISO-ALL.NG.WND.HL", PoundsOfCo2PerKwh = 0 });
             sources.Add(new HourlySource { Source = "Solar", SourceId = "EBA.MISO-ALL.NG.SUN.HL", PoundsOfCo2PerKwh = 0 });
             sources.Add(new HourlySource { Source = "Hydro", SourceId = "EBA.MISO-ALL.NG.WAT.HL", PoundsOfCo2PerKwh = 0 });
-            sources.Add(new HourlySource { Source = "Coal", SourceId = "EBA.MISO-ALL.NG.COL.HL", PoundsOfCo2PerKwh = int.MaxValue });
-            sources.Add(new HourlySource { Source = "Natural Gas", SourceId = "EBA.MISO-ALL.NG.NG.HL", PoundsOfCo2PerKwh = int.MaxValue });
-            sources.Add(new HourlySource { Source = "Nuclear", SourceId = "EBA.MISO-ALL.NG.NUC.HL", PoundsOfCo2PerKwh = int.MaxValue });
-            sources.Add(new HourlySource { Source = "Petro", SourceId = "EBA.MISO-ALL.NG.OIL.HL", PoundsOfCo2PerKwh = int.MaxValue });
-            sources.Add(new HourlySource { Source = "Other", SourceId = "EBA.MISO-ALL.NG.OTH.HL", PoundsOfCo2PerKwh = int.MaxValue });
+            sources.Add(new HourlySource { Source = "Coal", SourceId = "EBA.MISO-ALL.NG.COL.HL", PoundsOfCo2PerKwh = 2.23 });
+            sources.Add(new HourlySource { Source = "Natural Gas", SourceId = "EBA.MISO-ALL.NG.NG.HL", PoundsOfCo2PerKwh = 0.91 });
+            sources.Add(new HourlySource { Source = "Nuclear", SourceId = "EBA.MISO-ALL.NG.NUC.HL", PoundsOfCo2PerKwh = 0 });
+            sources.Add(new HourlySource { Source = "Petro", SourceId = "EBA.MISO-ALL.NG.OIL.HL", PoundsOfCo2PerKwh = 2.13 });
+            sources.Add(new HourlySource { Source = "Other", SourceId = "EBA.MISO-ALL.NG.OTH.HL", PoundsOfCo2PerKwh = 0.5 }); //What to use for other?
             return sources;
+        }
+
+        private class HourSummary
+        {
+            public DateTime Hour { get; set; }
+
+            public ICollection<HourlySource> Sources { get; } = new List<HourlySource>();
         }
 
         private class HourlySource
