d4589c0ea6d09a5dcdc5421d69997e26ea733652
ryan <ryan.policheri@science.doe.gov>
2022-11-15 22:34:54 -0600

    adjuster and subtractor node

---
+++ Tests.UIowaBuildingsModel/ProcessModelingTests/ProductSubtractorNodeTests.cs
@@ -0,0 +1,79 @@
+﻿using EmissionsMonitorModel.DataSources;
+using EmissionsMonitorModel.ProcessModeling;
+using EmissionsMonitorModel.TimeSeries;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnitsNet;
+
+namespace Tests.EmissionsMonitorModel.ProcessModelingTests
+{
+    [TestClass]
+    public class ProductSubtractorNodeTests
+    {
+        public class ProductDeductionFunctionHost
+        {
+            public Energy ProductDeduction(DataPoint OverheadPoint)
+            {
+                Energy overhead = Energy.FromMegabritishThermalUnits(OverheadPoint.Value);
+                return overhead;
+            }
+        }
+
+        [TestMethod]
+        public void WhenSubtractingProduct_CostsGoUpProportionally()
+        {
+            //MODEL
+            ExchangeNode boiler1ExchangeNode = SampleNodes.BuildBoiler1ExchangeNode();
+            ProductSubtractorNode deducterNode = new ProductSubtractorNode();
+            deducterNode.PrecedingNode = boiler1ExchangeNode;
+            deducterNode.ProductDeductionFunction = new SteamEnergyFunction()
+            {
+                FunctionName = "Product Deduction",
+                FunctionFactors = new List<FunctionFactor>()
+                    {
+                        new FunctionFactor() { FactorName = "Overhead", FactorUri = new DataSourceSeriesUri { Uri = "Overhead Tag" } }
+                    },
+                FunctionCode = "Foobar",
+                FunctionHostObject = new ProductDeductionFunctionHost(),
+            };
+
+            //DATA
+            ICollection<DataPoint> dataPoints = new List<DataPoint>();
+            dataPoints.Add(new DataPoint()
+            {
+                Series = new Series { SeriesUri = new DataSourceSeriesUri { Uri = "B1 NG Flow Tag" } },
+                Value = 5000 //$50 dollars of natural gas, 275.5 kgs of CO2
+            });
+            dataPoints.Add(new DataPoint()
+            {
+                Series = new Series { SeriesUri = new DataSourceSeriesUri { Uri = "B1 Steam Output Tag" } },
+                Value = 20 //20 MMBTU
+            });
+            dataPoints.Add(new DataPoint()
+            {
+                Series = new Series { SeriesUri = new DataSourceSeriesUri { Uri = "Overhead Tag" } },
+                Value = 5 // MMBTU
+            });
+
+            ProductCostResults deductedResults = deducterNode.RenderProductAndCosts(dataPoints);
+
+            deductedResults.Product.TotalValue.Should().Be(15);
+            deductedResults.Costs.First(x => x.UnitForm == "Money").TotalValue.Should().Be(50);
+            deductedResults.Costs.First(x => x.UnitForm == "CO2").TotalValue.Should().Be(275.5);
+
+            var perUnitCostsForDeduction = deductedResults.CalculateCostOfAbstractProductAmount(Energy.FromMegabritishThermalUnits(1));
+            Math.Round(perUnitCostsForDeduction.First(x => x.CostFunctionName == "Fuel Cost").Value, 3).Should().Be(3.333);
+            Math.Round(perUnitCostsForDeduction.First(x => x.CostFunctionName == "CO2 Emissions Cost").Value, 3).Should().Be(18.367);
+
+            //Make sure original is not impacted
+            ProductCostResults originalResults = boiler1ExchangeNode.RenderProductAndCosts(dataPoints);
+            originalResults.Product.TotalValue.Should().Be(20);
+            var perUnitCostsForOriginal = originalResults.CalculateCostOfAbstractProductAmount(Energy.FromMegabritishThermalUnits(1));
+            Math.Round(perUnitCostsForOriginal.First(x => x.CostFunctionName == "Fuel Cost").Value, 1).Should().Be(2.5);
+            Math.Round(perUnitCostsForOriginal.First(x => x.CostFunctionName == "CO2 Emissions Cost").Value, 3).Should().Be(13.775);
+        }
+    }
+}
---
+++ UIowaBuildingsModel/ProcessModeling/ProductSubtractorNode.cs
@@ -0,0 +1,44 @@
+﻿using EmissionsMonitorModel.TimeSeries;
+using System.Text.Json.Serialization;
+
+namespace EmissionsMonitorModel.ProcessModeling
+{
+    public class ProductSubtractorNode : ProcessNode
+    {
+        public ProductSubtractorNode()
+        {
+            PrecedingNodeId = -1;
+            PrecedingNode = null;
+        }
+
+        public int PrecedingNodeId { get; set; }
+
+        [JsonIgnore]
+        public ProcessNode PrecedingNode { get; set; }
+
+        public DataFunction ProductDeductionFunction { get; set; }
+
+        public override ICollection<DataFunction> GetUserDefinedFunctions()
+        {
+            return new List<DataFunction>() { ProductDeductionFunction };
+        }
+
+        public override ProductCostResults RenderProductAndCosts(ICollection<DataPoint> dataPoints)
+        {
+            ProductCostResults preceedingStream = PrecedingNode.RenderProductAndCosts(dataPoints);
+            if (preceedingStream.Product.UnitType != ProductDeductionFunction.FunctionUnit
+                || preceedingStream.Product.UnitForm != ProductDeductionFunction.FunctionUnitForm
+                || preceedingStream.Product.DefaultValueUnit != ProductDeductionFunction.FunctionDefaultReturnUnit)
+            {
+                throw new InvalidOperationException("The preceeding node and the product deduction function must have the same type of product");
+            }
+
+            var input = dataPoints.Where(x => ProductDeductionFunction.FunctionFactors.Any(y => y.FactorUri.Uri == x.Series.SeriesUri.Uri));
+            DataFunctionResult productDeduction = ProductDeductionFunction.ExecuteFunction(input);
+            if (preceedingStream.Product.TotalValue < productDeduction.TotalValue) { throw new InvalidOperationException("Deduction value cannot be more than total value"); }
+
+            preceedingStream.Product.TotalValue -= productDeduction.TotalValue;
+            return preceedingStream;
+        }
+    }
+}
